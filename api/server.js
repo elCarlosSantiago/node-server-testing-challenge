1b98fde7c098220314986bf36d3c4e8e97fafb4a
CarlosSantiago Nevett <csnevett@gmail.com>
2021-04-22 22:08:58 -0400

    artist router up

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const ArtistRouter = require('./artists/router');
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use('/api/artists', ArtistRouter);
 
 server.use('*', (req, res) => {
   res.status(404).json('Resource not found');
